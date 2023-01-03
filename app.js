72253b01518bd83a9c463bc88faa037580725d27
esaltzm <elisaltzman@gmail.com>
2023-01-03 10:43:51 -0500

    route for size in gb

--- app.js
+++ app.js
@@ -24,6 +24,23 @@ app.get('/count', (req, res) => {
 	})
 })
 
+app.get('/size', (req, res) => {
+	connection.query(`
+		SELECT 
+			table_name AS \`Table\`, 
+			round(((data_length + index_length) / 1024 / 1024 / 1000), 2) \`Size in GB\` 
+		FROM information_schema.TABLES 
+		WHERE table_schema = "weather_db"
+			AND table_name = "weather";
+	`, (error, results) => {
+		if (error) {
+			res.status(500).send(error)
+		} else {
+			res.send(results)
+		}
+	})
+})
+
 app.listen(3000, () => {
 	console.log('Listening on port 3000')
 })
